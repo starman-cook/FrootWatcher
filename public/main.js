2723d42190cef4d8bec259093a3c867dc2b5478f
Pavel Mishakov <pashamishakov@gmail.com>
2021-05-27 14:58:13 +0600

    project ready

---
+++ public/main.js
@@ -0,0 +1,124 @@
+const { app, BrowserWindow, Tray, Menu } = require('electron')
+const path = require('path')
+
+/**
+ * Переменная tray вынесена чтобы сборщики мусора не сносили иконку из taskBar
+ **/
+let tray
+function createWindow () {
+
+    /**
+     * Создание окна со всеми характеристиками
+     * nodeIntegration опасная хрень, открывает доступ ко всему, но без нее никак
+     **/
+    const win = new BrowserWindow({
+        height: 150,
+        width: 200,
+        frame: false,
+        resizable: false,
+        show: false,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
+    })
+
+    /**
+     * путь к главному html
+     * не ставлю проверку isDev, так как библиотеки слетают, лучше просто при package не забыть поменять и все))
+     **/
+    // win.loadURL('http://localhost:3000')
+    win.loadURL(path.join(__dirname, '../build/index.html'))
+
+    /**
+     * Функция если кликаем мимо открытого окна, чтобы закрыть это окно
+     * очень важная вещь, так как пользователи все абсолютно привыкли к подобному поведению подобных программ
+     **/
+    win.on('blur', () => {
+        win.hide()
+    })
+
+    /**
+     * Указываем пути к иконкам для таскбара (большой и маленькой)
+     * Проверка не имеет смысла пока что, но оставляю ее на случай если кто захочет разделить вид иконок для MacOS и Windows
+     **/
+    const iconName = process.platform === 'win32' ? "logo_main.png" : "logo_main.png"
+    const iconPath = path.join(__dirname, `./${iconName}`)
+    win.setIcon(iconPath)
+    tray = new Tray(iconPath)
+
+    /**
+     * При наведении на иконку увидим эту надпись
+     **/
+    tray.setToolTip('froot.kz screenshot watcher')
+
+    /**
+     * Событие открытия окна, здесь идет расчет координат где именно появится окно
+     * Есть проверка на Windows и MacOS
+     **/
+    tray.on('click', (event, bounds) => {
+        const {x, y} = bounds
+        const {height, width} = win.getBounds()
+        if (win.isVisible()) {
+            win.hide()
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y - height
+            win.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                height: height,
+                width: width
+            })
+            win.show()
+        }
+    })
+
+    /**
+     * Событие клика правой кнопки мыши
+     * Дает меню, где есть только кнопка Quit (выйти)
+     **/
+    tray.on('right-click', () => {
+        const menuConfig = Menu.buildFromTemplate([
+            {
+                label: 'Quit',
+                click: () => {
+                    app.quit()
+                }
+            }
+        ])
+
+        tray.popUpContextMenu(menuConfig)
+    })
+
+    /**
+     * В целом не очень нужная штука, все равно кнопки не видно, но событие на нажатие клавиш работает
+     * Command+Q или Ctrl+Q закрывают программу
+     * Но шлавная фишка этой штуки в том, что оно удаляет базовое меню и закрывает полностью доступ в dev tools чтобы пользователи не шарялись в кишках программы
+     **/
+    const menuTemplate = [
+                {
+                    label: 'Quit',
+                    accelerator: process.platform === 'darwin' ?'Command+Q' : 'Ctrl+Q',
+                    click() {
+                        app.quit()
+                    }
+                }
+    ]
+    const mainMenu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(mainMenu)
+}
+
+/**
+ * Базовые функции электрона для запуска программы, обычно везде такие они вот стандартные
+ **/
+app.on("ready", () => createWindow())
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
--- src/App.js
+++ src/App.js
@@ -1,24 +1,196 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import React, {useState, useEffect} from 'react';
+import "./App.css"
+import axios from "axios"
+const electron = window.require('electron');
+const { desktopCapturer } = electron
+
+/**
+ * Адресс для dev версии и рабочей
+ * Не стал использовать isDev, так как эти библиотеки постояно устаревают
+ **/
+// const apiURL = "http://localhost:8000";
+const apiURL = "http://162.55.54.115:8000";
 
 function App() {
+
+    /**
+     * Все необъодимые состояния, хранятся локально.
+     * ID пользователя хранится в localStorage, токенов никаких нет, так как
+     * для записи нужен лишь id
+     **/
+    const [inputLogin, setInputLogin] = useState({
+        workEmail: "",
+        password: ""
+    })
+    const [loader, setLoader] = useState(false)
+    const [error, setError] = useState(null)
+    const [user, setUser] = useState(localStorage.getItem("user"))
+    const [infoBB, setInfoBB] = useState({
+        merchant: ""
+    })
+    const [isStarted, setIsStarted] = useState(false)
+
+
+    /**
+     * Пользовательские вводы, один для логина
+     * Жругой для ввода имени (название компании наверно) поставщика
+    **/
+    const inputValue = (event) => {
+        setInputLogin(prevState => {
+            return {...prevState, [event.target.name]: event.target.value}
+        })
+    }
+    const inputMerchant = (event) => {
+        setError(null)
+        setInfoBB(prevState => {
+            return {...prevState, [event.target.name]: event.target.value}
+        })
+    }
+
+    /**
+     * Функция для проверки если предыдущая работа не была закончена
+     * То есть если пользователь нажал START, а потом закрыл программу или разлогинился
+     * То зайдя обратно там будет гореть кнопка STOP, чтобы завершить начатую работу
+     **/
+    const checkIfLast = async (userId) => {
+        setLoader(true)
+        try {
+            const response = await axios.get(apiURL + `/bigBrother/${userId}/lastJob`)
+            if (response.data.startScreen) {
+                setIsStarted(true)
+                setInfoBB(response.data)
+            } else {
+                setIsStarted(false)
+                setInfoBB({merchant: ""})
+            }
+            setLoader(false)
+            setError(false)
+        } catch (err) {
+            console.log(err)
+            setError(true)
+        }
+
+    }
+
+    /**
+     * Обычный переключатель вида экрана между START и STOP
+     **/
+    const toggleIsStarted = () => {
+        if (!error) {
+            setIsStarted(!isStarted)
+        }
+    }
+
+
+    /**
+     * ГЛАВНАЯ ФУНКЦИЯ
+     * Здесь происходит все то, ради чего работает программа
+     * Снимается скриншот (можно снимать также и видео и аудио с устройства, но здесь только скриншот)
+     * Собирается FormData с картинкой формата base64, id пользователя и именем поставщика
+     * И все это отправляется на сервер
+     **/
+    const takeAScreenShot = async () => {
+        if (infoBB.merchant.trim() === "") {
+            setError("Merchant name is required")
+            return
+        }
+         desktopCapturer.getSources({
+            types: ['screen'],
+            thumbnailSize: {
+                width: 1280,
+                height: 800
+            }
+        })
+            .then( async sources => {
+                setLoader(true)
+                const screenshotBase64 = await sources[0].thumbnail.toDataURL()
+                try {
+                    const formData = new FormData()
+                    formData.append("image", screenshotBase64)
+                    formData.append("merchant", infoBB.merchant)
+                    formData.append("user", user)
+                    await axios.post(apiURL + '/bigBrother', formData)
+                    toggleIsStarted()
+                    setError(null)
+                    setLoader(false)
+                } catch {
+                    setError("No internet connection((((")
+                }
+            })
+    }
+
+    /**
+     * Логаут пользователя, удаляет id из localStorage
+     **/
+    const logOut = () => {
+        localStorage.clear()
+        setUser(null)
+    }
+    /**
+     * Логин, отправляет имэйл и пароль на сервер, если все хорошо, то присылает в ответ данные пользователя
+     **/
+    const submitHandler = async (event) => {
+        event.preventDefault()
+        try {
+            const response = await axios.post(apiURL + "/users/electron", inputLogin)
+            localStorage.setItem("user", response.data.user._id)
+            setUser(response.data.user._id)
+            await checkIfLast(response.data.user._id)
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    /**
+     * Проверка на незаконченную заявку при запуске программы. Даже когда еще пользователь не залогинился идет проверка
+     * Возможно это стоит убрать, еще не решил, пока проблем с этим нет. А вот если добавлять кучу проверок когда именно запускать проверки на незаконченную работу
+     * то тогда может и начнутся проблемы)))
+     **/
+    useEffect(() => {
+        setUser(localStorage.getItem("user"))
+        checkIfLast(user)
+    }, [])
+    let main
+
+    /**
+     * Отрисовка экрана
+     * Если есть id юзера, то видим START STOP экран
+     * А если нет, то видим форму логина
+     **/
+    if (user) {
+        main = (
+            <div className={"BigBrother__bg--top"}>
+                {!isStarted ? <input required value={infoBB.merchant} name={"merchant"} onChange={(event) => {inputMerchant(event)}} placeholder={"merchant"} className={"BigBrotherInput"} type={"text"} /> : null}
+                {infoBB.merchant.length && isStarted ? <p className={"BigBrother__merchant"}>{infoBB.merchant}</p> : null}
+                {/*<img id={"screenshot-image"} />*/}
+                {/*<div onClick={bigBrotherDoYourJob} id={"BigBrother"} style={isStarted ? {'background': 'red'} : {'background': 'green'}} className={"BigBrother"}>*/}
+                <div onClick={takeAScreenShot} id={"BigBrother"} className={"BigBrother"}>
+                    <img className={"BigBrother__image"} src={isStarted ? "./red.gif" : "./green.gif"} />
+                    {!error ? <h1 className={"BigBrother__text"}>{isStarted ? "STOP" : "START"}</h1>
+                        : <h1 className={"BigBrother__text--error"}>{error}</h1>}
+                </div>
+                <button onClick={logOut} className={"BigBrotherInput--button BigBrotherInput--button--positioning"}>Log out</button>
+                {loader ? <div className={"Loader"} /> : null}
+            </div>
+        )
+    } else {
+        main = (
+            <div className={"BigBrother__bg--top"}>
+                <form onSubmit={(event) => {submitHandler(event)}} className={"BigBrother__form"}>
+                    <input required name={"workEmail"} onChange={(event) => {inputValue(event)}} placeholder={"work email"} className={"BigBrotherInput BigBrotherInput--login"} type={"text"} />
+                    <input required name={"password"} onChange={(event) => {inputValue(event)}} placeholder={"password"} className={"BigBrotherInput BigBrotherInput--login"} type={"password"} />
+                    <button className={"BigBrotherInput--button"} type={"submit"}>Login</button>
+                </form>
+            </div>
+        )
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          {main}
+      </>
   );
 }
 
